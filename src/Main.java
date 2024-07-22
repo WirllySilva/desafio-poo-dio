d6c0b3dea70d8555655928a7471a5a8373fdc4cc
Wirlly Silva <wirlly.silva@gmail.com>
2024-07-22 14:02:15 -0300

    New classes were added to the project.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso java poo");
+        curso1.setCargaHoraria(10);
+
+        System.out.println(curso1);
+
     }
 }
